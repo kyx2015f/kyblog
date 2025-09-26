46cfddc511ead282a7046dfca93123e3967fbfa1
C14111035 <c14111035@gs.ncku.edu.tw>
2025-09-27 03:06:01 +0800

    新增文章：文章標題或修改內容

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -168,7 +168,7 @@ const config = {
             ],*/
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} ky's blog.`,
       },
       prism: {
         theme: prismThemes.github,
